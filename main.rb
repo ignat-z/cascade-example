d411d4847d6a0d726f04841004632c36fbec3aa1
Ignat Zakrevsky <iezakrevsky@gmail.com>
2015-04-06 05:27:57 +0300

    Removed data saver

--- main.rb
+++ main.rb
@@ -1,11 +1,8 @@
 require 'cascade'
-require 'awesome_print'
-
-PUTS_DATA_SAVER = ->(*args) { ap args }
 
 Cascade.configuration do
   Cascade::RowProcessor.use_default_presenter = false
   Cascade::ColumnsMatching.mapping_file = "columns_mapping.yml"
 end
 
-Cascade::DataParser.new("data_test.txt", data_saver: PUTS_DATA_SAVER).call
+Cascade::DataParser.new("data_test.txt").call
