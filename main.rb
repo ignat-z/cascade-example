1b89e75cfcc74ff8a93cb15b77dfcc444ae7cbea
Ignat Zakrevsky <iezakrevsky@gmail.com>
2015-11-06 16:43:29 +0300

    Migrated CascadeCsv from gem to example

--- main.rb
+++ main.rb
@@ -1,8 +1,9 @@
 require 'cascade'
+require_relative 'cascade_csv'
 
 Cascade.configuration do
   Cascade::RowProcessor.use_default_presenter = false
   Cascade::ColumnsMatching.mapping_file = "columns_mapping.yml"
 end
 
-Cascade::DataParser.new("data_test.txt").call
+Cascade::DataParser.new("data_test.txt", data_provider: CascadeCsv).call
